5181703673080cc89f5a98dbd60613d79cd3a134
Gabriel <dev6@borial.fr>
2020-10-28 04:25:47 +0100

    update

--- src/api/index.js
+++ src/api/index.js
@@ -6,7 +6,7 @@ export function getAllCategorie(){
     axios.get(baseUrl + '/rest/fr_FR/V1/categories/', {
       headers: {
           'Authorization': 'Bearer ' + accessToken,
-          "Content-Type": "application/x-www-form-urlencoded"
+          "content-type": "application/octet-stream",
       }
     })
     .then((res) => resolve(res))
